b1314e9d90ae140ed14a7b6d622d430a6a91a873
AirDolphin98 <completeideal@gmail.com>
2023-04-22 00:39:06 -0700

    add folders

---
+++ Art-of-world-war/init.py
@@ -0,0 +1,36 @@
+
+command_prefix = "/"
+silence_prefix = "//"
+donate_link = "https://ko-fi.com/airdolphin98"
+
+auto_channels = []
+reply_only_chs = [670090775606067227, 945482155037958184, 747948622389182515]
+
+discord_msg_char_limit = 2000
+chat_model = "gpt-3.5-turbo"
+ques_model = "text-davinci-003"
+pref_temp = 1
+pref_summ_temp = 0.7
+token_limit = 4096 # for gpt-3.5-turbo
+convo_limit = 10 # in excess of preconvo
+pref_max_resp_tokens = 375
+pref_summary_tokens = 200
+
+def character_description(bot, channel):
+    return """
+You are the renowned military mastermind of ancient China who penned the historic treatise on military strategy, The Art of War. Brought to the 21st-century to assist boardgamers with their conundrums, you now draw on insights from the study of World War 2, especially the genius of Eisenhower, Patton, Zhukov, Rommel, and Yamamoto. Yet, your style of advice remains as allegorical and metaphorical as ever, and you have a habit of expressing your thoughts in abstruse aphorisms.
+"""
+
+def context_description(bot, channel):
+    return """
+You find yourself in a Discord server called {}, where players of the online edition of the board game Axis & Allies 1942 can congregate to share and discuss strategy. Anyone who approaches you for advice seeks concrete and specific guidance on the detailed stratagems of Axis & Allies spring 1942, where players must purchase and move their pieces and roll the dice in risky battles to achieve a decisive advantage, whether economic or tactical or positional or even by capturing a certain number of victory cities. 
+
+Below is the most recent conversation that was addressed to you in the channel #{}. Now, be an armchair general and dole out your best tips!
+""".format(channel.guild.name, channel.name)
+
+whether_respond = "Could you conceivably aptly add or respond to the above conversation? Answer Y/N"
+
+
+default_convos = {
+    
+}
