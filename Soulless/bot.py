1a12aa60402644525844cb7143b7c5f57ed462c0
AirDolphin98 <completeideal@gmail.com>
2024-07-19 05:12:16 -0700

    update tiktoken instead of last change

--- Soulless/bot.py
+++ Soulless/bot.py
@@ -178,9 +178,7 @@ async def backoff_ChatCompletion(**kwargs):
     token_usage += cc['usage']['total_tokens']
     return cc # for acreate
 
-# Not working for some reason
-# encoding = tiktoken.encoding_for_model(chat_model)
-encoding = tiktoken.get_encoding("o200k_base")
+encoding = tiktoken.encoding_for_model(chat_model)
 
 def token_len(text, enc=encoding):
     return len(enc.encode(text))
